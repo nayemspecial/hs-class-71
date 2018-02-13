9df7a872393197f03c191d039e220fc4e6a4f170
nayemspecial <nayemspecial@gmail.com>
2018-02-13 17:07:05 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+        
+        
+    });
+
+
+    jQuery(window).load(function(){
+		
+		
+    });
+
+
+}(jQuery));	
